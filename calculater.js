8d9c477a1af0e1a9692397316ede293a99e6c497
nityansh01 <nityansh012000@gmail.com>
2025-02-26 09:31:22 +0530

    add calculator

---
+++ calculater.js
@@ -0,0 +1,31 @@
+let b=document.getElementById("outerscreen").value;
+
+
+
+
+function one(n){
+  document.getElementById("outerscreen").value+=n;
+}
+
+
+// function equal(){
+//   try {outerscreen.value=eval(outerscreen.value); }  
+//  catch (error) { alert("alert "); }
+//  document.getElementById("outerscreen").value=result; }
+    
+
+// function ac(){
+//     outerscreen.value="";
+// }
+
+function equal(){
+    try{ outerscreen.value=eval(outerscreen.value);}
+    catch(error){alert('alert')}
+    document.getElementById("ouerscreen").value=result;
+}
+
+function  Del(){
+    outerscreen.value =outerscreen.value.slice(0,-1);
+    
+}
+
